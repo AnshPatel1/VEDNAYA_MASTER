b266a21b342bbb80137b5af8cfaa7b6b1b11e4b2
Ansh Patel <1210anshpatel@gmail.com>
2021-10-04 17:11:45 +0530

    added views in views.py

--- api_app/views.py
+++ api_app/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
-from .models import Product
-from .serializers import ProductSerializer
+from .models import *
+from .serializers import *
 
 
 class ProductList(generics.ListCreateAPIView):
@@ -13,3 +13,63 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ProductSerializer
+
+
+class HQList(generics.ListCreateAPIView):
+    queryset = HQ.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = HQSerializer
+
+
+class HQDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = HQ.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = HQSerializer
+
+
+class DoctorList(generics.ListCreateAPIView):
+    queryset = Doctor.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = DoctorSerializer
+
+
+class ARCList(generics.ListCreateAPIView):
+    queryset = ARC.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = ARCSerializer
+
+
+class ARCDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ARC.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ARCSerializer
+
+
+class DoctorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Doctor.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = DoctorSerializer
+
+
+class ChemistList(generics.ListCreateAPIView):
+    queryset = Chemist.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = ChemistSerializer
+
+
+class ChemistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Chemist.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ChemistSerializer
+
+
+class MSOList(generics.ListCreateAPIView):
+    queryset = MSO.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = MSOSerializer
+
+
+class MSODetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MSO.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = MSOSerializer
